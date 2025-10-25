586817ecf6b48c51b597fdad2e53fe7f400a4b97
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-10-26 04:53:53 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
+
 from fastapi import FastAPI
 from app.routers import text, url, pdf
 
-app = FastAPI(title="Trust Guard")
+app = FastAPI(title="Authenticity Checker")
 
 app.include_router(text.router)
 app.include_router(url.router)
@@ -10,3 +15,4 @@ app.include_router(pdf.router)
 @app.get("/")
 def root():
     return {"status": "ok"}
+
