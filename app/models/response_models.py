f9a2c5ab1a61418d9f978b9c873a77aac25eb928
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-10-25 22:18:36 +0530

    Update response_models.py

--- app/models/response_models.py
+++ app/models/response_models.py
@@ -1,1 +1,34 @@
+from pydantic import BaseModel
+from typing import List
 
+class SourceInfo(BaseModel):
+    title: str
+    url: str
+
+class LLMOpinions(BaseModel):
+    chatgpt: str
+    gemini: str
+
+class TextVerificationResponse(BaseModel):
+    input_type: str = "text"
+    authenticity_score: int
+    sources_analyzed: int
+    sources: List[SourceInfo]
+    llm_opinions: LLMOpinions
+    final_summary: str
+
+class URLVerificationResponse(BaseModel):
+    input_type: str = "url"
+    authenticity_score: int
+    sources_analyzed: int
+    sources: List[SourceInfo]
+    llm_opinions: LLMOpinions
+    final_summary: str
+
+class PDFVerificationResponse(BaseModel):
+    input_type: str = "pdf"
+    authenticity_score: int
+    sources_analyzed: int
+    sources: List[SourceInfo]
+    llm_opinions: LLMOpinions
+    final_summary: str
