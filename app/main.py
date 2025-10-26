823e828200ada59ea82834576a17749394ac265c
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-10-26 06:09:04 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,22 +7,24 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import text, url, pdf
 
-app = FastAPI(title="Truth Guard")
-
+app = FastAPI(title="truth guard")
 origins = [
-    "https://truthguard-pearl.vercel.app",
+    "http://localhost:3000", 
+    "https://truthguard-pearl.vercel.app/", 
+]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins, 
     allow_credentials=True,
-    allow_methods=["*"],    
-    allow_headers=["*"],  
+    allow_methods=["*"],  
+    allow_headers=["*"],    
 )
+
 app.include_router(text.router)
 app.include_router(url.router)
 app.include_router(pdf.router)
 
 @app.get("/")
 def root():
-    return {"status": "ping pong"}
+    return {"status": "chal raha hai yoo^^"}
