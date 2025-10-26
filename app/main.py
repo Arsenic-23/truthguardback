b8eb3f17ad4a9c383eb13ff2a0ba893a48dffa81
Arsenic <156888342+Arsenic-23@users.noreply.github.com>
2025-10-26 06:39:21 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,26 @@
 from dotenv import load_dotenv
 import os
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.routers import text, url, pdf 
 
 load_dotenv()
 
-app = FastAPI(title="truth guard")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import text, url, pdf
 
+app = FastAPI(title="Authenticity Checker")
+
+
+origins = [
+    "http://localhost:3000",               
+    "https://truthguard-pearl.vercel.app/",   
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://truthguard-pearl.vercel.app/"],  
+    allow_origins=origins, 
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],    
+    allow_headers=["*"],  
 )
 
 app.include_router(text.router)
@@ -23,4 +29,4 @@ app.include_router(pdf.router)
 
 @app.get("/")
 def root():
-    return {"status": "ping pong"}
+    return {"status": "yoo^^"}
